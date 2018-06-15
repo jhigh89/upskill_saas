655ec0a9b1f1d2bfa66428cca016c49f68dbebc1
Jacob Hightower <jah795@nau.edu>
2018-06-15 08:29:17 +0000

    Added length constraints to contact validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,8 @@
 class Contact < ActiveRecord::Base
-    validates :name, presence: true
-    validates :email, presence: true
-    validates :comments, presence: true
+    validates :name, presence: true, length: {minimum:1, maximum:30}
+    
+    validates :email, presence: true, length: {minimum:1, maximum:75}
+    
+    validates :comments, presence: true, length: {minimum:2, maximum:750, 
+    message:"750 is the max value for comments. :)"}
 end
